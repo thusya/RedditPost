685c1d6d84648860306c0a561ae40200f456ed94
Thuseeman <thushfit@gmail.com>
2023-06-23 01:00:31 +0100

    Reformat the code

--- app/src/test/java/com/thus/redditpost/ui/util/UtilsTest.kt
+++ app/src/test/java/com/thus/redditpost/ui/util/UtilsTest.kt
@@ -5,39 +5,39 @@ import org.junit.Test
 
 class UtilsTest {
 
-@Test
-fun `formatCount returns count as string when count is less than 1000`() {
-    // Given
-    val count = 500
+    @Test
+    fun `formatCount returns count as string when count is less than 1000`() {
+        // Given
+        val count = 500
 
-    // When
-    val formattedCount = Utils.formatCount(count)
+        // When
+        val formattedCount = Utils.formatCount(count)
 
-    // Then
-    assertEquals("500", formattedCount)
-}
+        // Then
+        assertEquals("500", formattedCount)
+    }
 
-@Test
-fun `formatCount returns formatted count with K suffix when count is between 1000 and 999999`() {
-    // Given
-    val count = 1500
+    @Test
+    fun `formatCount returns formatted count with K suffix when count is between 1000 and 999999`() {
+        // Given
+        val count = 1500
 
-    // When
-    val formattedCount = Utils.formatCount(count)
+        // When
+        val formattedCount = Utils.formatCount(count)
 
-    // Then
-    assertEquals("1.5 K", formattedCount)
-}
+        // Then
+        assertEquals("1.5 K", formattedCount)
+    }
 
-@Test
-fun `formatCount returns formatted count with M suffix when count is 1000000 or greater`() {
-    // Given
-    val count = 2500000
+    @Test
+    fun `formatCount returns formatted count with M suffix when count is 1000000 or greater`() {
+        // Given
+        val count = 2500000
 
-    // When
-    val formattedCount = Utils.formatCount(count)
+        // When
+        val formattedCount = Utils.formatCount(count)
 
-    // Then
-    assertEquals("2.5 M", formattedCount)
-}
+        // Then
+        assertEquals("2.5 M", formattedCount)
+    }
 }
